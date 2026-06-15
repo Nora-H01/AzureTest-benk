137897608743c42b99c557c1ddddf1109c18002c
Nora H. <nora.hashani01@gmail.com>
2026-06-15 11:46:08 +0200

    Ajoutez des fichiers projet.

---
+++ AzureTest-benk/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureTest_benk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from GameController!");
+        }
+    }
+}
